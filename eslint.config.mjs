dd91aed561bcf3f41e665d8ef16a82a993235366
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-05-27 13:52:18 -0500

    feat: add Prettier and ESLint configurations for improved code formatting and linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,21 @@
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
 import importSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettier,
   {
     plugins: {
       'simple-import-sort': importSort,
       'unused-imports': unusedImports,
-      'jsx-a11y': jsxA11y,
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
       "simple-import-sort/imports": [
         "error",
         {
@@ -44,15 +38,8 @@ const eslintConfig = [
       'simple-import-sort/exports': 'error',
       'unused-imports/no-unused-imports': 'warn',
       'jsx-a11y/alt-text': 'warn',
-    },
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    }
+  }
 ];
 
 export default eslintConfig;
